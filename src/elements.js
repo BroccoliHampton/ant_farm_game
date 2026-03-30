0909c4eafa83054425db23c4920a0921960dcefa
BroccoliHampton <137211602+BroccoliHampton@users.noreply.github.com>
2026-03-30 14:31:32 -0700

    Update elements.js

--- src/elements.js
+++ src/elements.js
@@ -365,7 +365,6 @@ export function stepProgVoid(x,y,p){
 }
 
 // Weather station state
-let ws_rain_active=false, ws_rain_rate=3;
 let ws_rain_type_key='water'; // key for element
 export function wsRainType(){ return ({water:T.WATER,acid:T.ACID,sand:T.SAND,lava:T.LAVA,oil:T.OIL,salt:T.SALT,ice:T.ICE,fire:T.FIRE,steam:T.STEAM,ash:T.ASH,smoke:T.SMOKE,gunpowder:T.GUNPOWDER,detritus:T.DETRITUS})[ws_rain_type_key]||T.WATER; }
 
